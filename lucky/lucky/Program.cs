f13d0f0b205718d48fc56687052a8a61178490d1
Logan Benson <129000785+Walacup@users.noreply.github.com>
2024-06-06 12:39:07 -0400

    Basic room

--- lucky/lucky/Program.cs
+++ lucky/lucky/Program.cs
@@ -11,6 +11,12 @@ public class Program
     static int playerSize = 60; // Player size
     static int playerSpeed = 5; // Player movement speed
     static Color backgroundColor = new Color(85, 92, 61, 255); // Custom background color
+    static Color playerColor = new Color(158, 115, 62, 255); // Player color (#9e733e)
+    static Color bedColor = new Color(117, 47, 57, 255); // Bed color (#752f39)
+    static Color deskColor = new Color(61, 51, 37, 255); // Desk color (#3d3325)
+    static Color wardrobeColor = new Color(128, 90, 41, 255); // Wardrobe color (#805a29)
+    static Color chairColor = new Color(140, 108, 76, 255); // Chair color (#8c6c4c)
+    static Color floorColor = new Color(33, 24, 14, 255); // Custom color (#21180e)
 
     static void Main()
     {
@@ -60,18 +66,25 @@ public class Program
     static void DrawRoom()
     {
         // Draw the room floor
-        Raylib.DrawRectangle(100, 100, screenWidth - 200, screenHeight - 200, Raylib_cs.Color.Brown);
+        Raylib.DrawRectangle(100, 100, screenWidth - 200, screenHeight - 200, Raylib_cs.Color.RayWhite);
+
+        // Draw the room border with increased thickness
+        int borderThickness = 10;
+        Raylib.DrawRectangle(90, 90, screenWidth - 180, borderThickness, Raylib_cs.Color.Black); // Top border
+        Raylib.DrawRectangle(90, screenHeight - 110, screenWidth - 180, borderThickness, Raylib_cs.Color.Black); // Bottom border
+        Raylib.DrawRectangle(90, 90, borderThickness, screenHeight - 180, Raylib_cs.Color.Black); // Left border
+        Raylib.DrawRectangle(screenWidth - 110, 90, borderThickness, screenHeight - 180, Raylib_cs.Color.Black); // Right border
 
         // Draw furniture (example)
-        Raylib.DrawRectangle(150, 150, 300, 150, Raylib_cs.Color.DarkGray); // Bed
-        Raylib.DrawRectangle(500, 250, 150, 75, Raylib_cs.Color.DarkGray); // Desk
-        Raylib.DrawRectangle(700, 150, 75, 200, Raylib_cs.Color.DarkGray); // Wardrobe
-        Raylib.DrawRectangle(900, 400, 150, 150, Raylib_cs.Color.DarkGray); // Chair
+        Raylib.DrawRectangle(150, 130, 300, 150, bedColor); // Bed
+        Raylib.DrawRectangle(675, 130, 150, 75, deskColor); // Desk
+        Raylib.DrawRectangle(545, 130, 75, 200, wardrobeColor); // Wardrobe
+        Raylib.DrawRectangle(725, 210, 50, 50, deskColor); // Chair
     }
 
     static void DrawPlayer()
     {
         // Draw the player
-        Raylib.DrawRectangleV(playerPosition, new Vector2(playerSize, playerSize), Raylib_cs.Color.Black);
+        Raylib.DrawRectangleV(playerPosition, new Vector2(playerSize, playerSize), playerColor);
     }
 }
